dd787e48b6a541fc757645c8f1b8a18bc1b43ead
yuma2ok <yuma.matsuoka25@gmail.com>
2022-03-07 07:31:27 +0000

    signup 実装中

--- app.rb
+++ app.rb
@@ -4,6 +4,35 @@ require 'sinatra/reloader' if development?
 require './models'
 require 'dotenv/load'
 
+enable :sessions
+
 get '/' do
     erb :index
+end
+
+get '/home' do
+    erb :home
+end
+
+get '/search' do
+    erb :search
+end
+
+get '/signup' do
+    erb :sign_up
+end
+
+post '/signup' do
+    @user = User.create(mail:params[:mail], password:params[:password],
+    password_confirmation:params[:password_confirmation], file:params[:file])
+    if @user.persisted?
+        session[:user] = @user.id
+    end
+    redirect '/'
+end
+
+
+get '/signout' do
+    session[:user] = nil
+    redirect '/'
 end
--- models.rb
+++ models.rb
@@ -1,4 +1,6 @@
 require 'bundler/setup'
 Bundler.require
 
-ActiveRecord::Base.establish_connection
+class User < ActiveRecord::Base.establish_connection
+    
+end
